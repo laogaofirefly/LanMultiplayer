7b743b4d6d53686689f8a15622cd716a7dd11586
laogaofirefly <gao520fireflt@gmail.com>
2026-08-09 14:01:25 +0000

    Opt in to experimental Material 3 top app bar API

--- app/src/main/java/com/example/lanmultiplayer/LanScreen.kt
+++ app/src/main/java/com/example/lanmultiplayer/LanScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LanScreen(viewModel: LanViewModel) {
     val rooms by viewModel.rooms.collectAsStateWithLifecycle()
